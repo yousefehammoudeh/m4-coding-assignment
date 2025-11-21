14c1dbd14e4b23928bb66741dd18003b5ed36ecd
Yousef Hammoudeh <yousefhammoudeh@Yousefs-MacBook-Pro.local>
2025-11-21 12:05:13 -0500

    Task 4.2 - Moved amountFor and volumeCredits calculations to AbstractPerformanceCalculator

--- src/main/java/theater/AbstractPerformanceCalculator.java
+++ src/main/java/theater/AbstractPerformanceCalculator.java
@@ -29,5 +29,38 @@ public abstract class AbstractPerformanceCalculator {
         };
     }
 
-    public abstract int amountFor();
+    public int amountFor() {
+        int result;
+        switch (play.getType()) {
+            case "tragedy":
+                result = Constants.TRAGEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                }
+                break;
+            case "comedy":
+                result = Constants.COMEDY_BASE_AMOUNT;
+                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                    result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                                    * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                }
+                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+                break;
+            default:
+                throw new RuntimeException(String.format("unknown type: %s", play.getType()));
+        }
+        return result;
+    }
+
+    public int volumeCredits() {
+        int result = 0;
+        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        // add extra credit for every five comedy attendees
+        if ("comedy".equals(play.getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
+    }
 }
--- src/main/java/theater/StatementData.java
+++ src/main/java/theater/StatementData.java
@@ -32,43 +32,10 @@ public class StatementData {
     private PerformanceData createPerformanceData(Performance performance) {
         Play play = plays.get(performance.getPlayID());
         AbstractPerformanceCalculator calculator = AbstractPerformanceCalculator.createPerformanceCalculator(performance, play);
-        int amount = calculateAmount(performance, play);
-        int volumeCredits = calculateVolumeCredits(performance, play);
+        int amount = calculator.amountFor();
+        int volumeCredits = calculator.volumeCredits();
         return new PerformanceData(play.getName(), performance.getAudience(), 
                 play.getType(), amount, volumeCredits);
-    }    private int calculateAmount(Performance performance, Play play) {
-        int result;
-        switch (play.getType()) {
-            case "tragedy":
-                result = Constants.TRAGEDY_BASE_AMOUNT;
-                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
-                    result += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
-                }
-                break;
-            case "comedy":
-                result = Constants.COMEDY_BASE_AMOUNT;
-                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
-                    result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
-                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                                    * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
-                }
-                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
-                break;
-            default:
-                throw new RuntimeException(String.format("unknown type: %s", play.getType()));
-        }
-        return result;
-    }
-
-    private int calculateVolumeCredits(Performance performance, Play play) {
-        int result = 0;
-        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-        // add extra credit for every five comedy attendees
-        if ("comedy".equals(play.getType())) {
-            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-        }
-        return result;
     }
 
     public int totalAmount() {
